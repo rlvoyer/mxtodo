c4d5d7279621b49b3a0c033d131a0288df7ac4bc
Robert Voyer <robert@descarteslabs.com>
2021-07-04 07:36:10 -0700

    Cleanup tests
    Closes #7.

--- test/test.el
+++ test/test.el
@@ -3,10 +3,11 @@
 (require 'ert)
 (require 'ts)
 
-(require 'todo-search)
+(require 'mxtodo)
 
 (ert-deftest test-display-date-from-file-path ()
-  (should (equal (todo-search--display-date-from-file-path "/foo/bar/2021-5-24.md")
+  "Tests the construction of a TODO display date from its filename."
+  (should (equal (mxtodo--display-date-from-file-path "/foo/bar/2021-5-24.md")
                  (make-ts 
                   :year 2021
                   :month 5
@@ -16,6 +17,7 @@
                   :second 0))))
 
 (ert-deftest test-render-a-todo-date ()
+  "Tests that a TODO renders correctly."
   (let ((date (make-ts 
                :year 2021
                :month 5
@@ -23,12 +25,13 @@
                :hour 0
                :minute 0
                :second 0)))
-    (should (equal (todo-search--render-date date)
+    (should (equal (mxtodo--render-date date)
                    "2021-5-24"))))
 
 (ert-deftest test-make-todo-from-temp-file-line ()
+  "Tests that a TODO can be constructed from a temp file line."
   (let ((expected
-         (make-todo-search-item :file-path "/Users/robertvoyer/Documents/Notes/2021-6-24.md"
+         (make-mxtodo-item :file-path "/Users/robertvoyer/Documents/Notes/2021-6-24.md"
                                 :file-line-number 10
                                 :file-display-date-ts (make-ts :year 2021
                                                                :month 6
@@ -40,13 +43,15 @@
                                 :text "write some unit tests"
                                 :is-completed t))
         (actual
-         (todo-search--make-todo-from-temp-file-line  "/Users/robertvoyer/Documents/Notes/2021-6-24.md	10	- [x] write some unit tests	1624637870")))
+         (mxtodo--make-todo-from-temp-file-line  "/Users/robertvoyer/Documents/Notes/2021-6-24.md	10	- [x] write some unit tests	1624637870")))
     (should (equal expected actual))))
 
 (defun todo-text-no-properties (rendered-todo-item)
+  "Test helper function that renders a TODO as a string with no properties."
   (substring-no-properties rendered-todo-item 0 (next-single-property-change 0 'invisible rendered-todo-item)))
 
 (ert-deftest test-render-date ()
+  "Tests that a TODO date renders correctly."
   (let* ((date
           (make-ts :year 2021
                    :month 6
@@ -55,32 +60,23 @@
                    :minute 0
                    :second 0)) 
          (expected "2021-6-24")
-         (actual (todo-search--render-date date)))    
+         (actual (mxtodo--render-date date)))
     (should (equal expected actual))))
 
 (ert-deftest test-render-todo ()
+  "Tests that a TODO renders correctly."
   (let* ((input-todo
-          (make-todo-search-item :file-path "/Users/robertvoyer/Documents/Notes/2021-6-24.md"
-                                 :file-line-number 10
-                                 :file-display-date-ts (make-ts :year 2021
-                                                                :month 6
-                                                                :day 24
-                                                                :hour 0
-                                                                :minute 0
-                                                                :second 0)
-                                 :file-last-update-ts (make-ts :unix 1624637870)
-                                 :text "write some unit tests"
-                                 :is-completed nil))
+          (make-mxtodo-item :file-path "/Users/robertvoyer/Documents/Notes/2021-6-24.md"
+                            :file-line-number 10
+                            :file-display-date-ts (make-ts :year 2021
+                                                           :month 6
+                                                           :day 24
+                                                           :hour 0
+                                                           :minute 0
+                                                           :second 0)
+                            :file-last-update-ts (make-ts :unix 1624637870)
+                            :text "write some unit tests"
+                            :is-completed nil))
          (expected "- [ ] write some unit tests (created 2021-6-24 / )")
-         (actual (todo-text-no-properties (todo-search--render-todo input-todo))))
+         (actual (todo-text-no-properties (mxtodo--render-todo input-todo))))
     (should (equal expected actual))))
-
-
-(ert-deftest test-todo-completed-from-todo-text ()
-  (should (equal (todo-search-todo-completed-p "- [x] buy groceries") t)))
-
-;; (ert-deftest test-todo-toggle-changes-incomplete-to-complete ()
-;;   (let* ((actual (make-todo-search-item))
-;;          (expected (copy-todo-search-item actual)))
-;;     (setf (todo-search-item-is-completed expected) t)
-;;     (should (equal expected (todo-search--toggle-todo-completed actual)))))
