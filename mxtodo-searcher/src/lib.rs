b4f84dfffd079afaa88d107d0033130ab78bdf3d
Robert Voyer <robert.voyer@gmail.com>
2021-09-25 16:41:51 -0700

    Rust format

--- mxtodo-searcher/src/lib.rs
+++ mxtodo-searcher/src/lib.rs
@@ -1,13 +1,14 @@
-#[macro_use] extern crate failure;
+#[macro_use]
+extern crate failure;
 
 use std::ffi::OsString;
 use std::io;
 
+use emacs::{defun, Env, Value};
 use failure::Error;
 use grep::{regex::RegexMatcherBuilder, searcher::BinaryDetection, searcher::SearcherBuilder};
-use walkdir::WalkDir;
-use emacs::{Env, Value, defun};
 use serde::Serialize;
+use walkdir::WalkDir;
 
 // Emacs won't load the module without this.
 emacs::plugin_is_GPL_compatible!();
@@ -37,7 +38,7 @@ fn files_to_search(directory: String, file_ext: String) -> Vec<OsString> {
 pub struct TodoMatch {
     pub file_path: String,
     pub line_number: u64,
-    pub line: String
+    pub line: String,
 }
 
 impl TodoMatch {
@@ -45,12 +46,16 @@ impl TodoMatch {
         TodoMatch {
             file_path: file_path.to_string_lossy().to_string(),
             line_number,
-            line: line.trim_end_matches(&['\r', '\n'][..]).to_string()
+            line: line.trim_end_matches(&['\r', '\n'][..]).to_string(),
         }
     }
 }
 
-pub fn _search_directory(directory: String, file_ext: String, pattern: String) -> Result<Vec<TodoMatch>, Error> {
+pub fn _search_directory(
+    directory: String,
+    file_ext: String,
+    pattern: String,
+) -> Result<Vec<TodoMatch>, Error> {
     let matcher = RegexMatcherBuilder::new()
         .multi_line(true)
         .line_terminator(Some(b'\n'))
@@ -74,8 +79,7 @@ pub fn _search_directory(directory: String, file_ext: String, pattern: String) -
 
             let results_sink = grep::searcher::sinks::UTF8(capture_results);
 
-            searcher
-                .search_path(&matcher, &file_path, results_sink)?;
+            searcher.search_path(&matcher, &file_path, results_sink)?;
 
             Ok(results)
         })
@@ -86,14 +90,17 @@ pub fn _search_directory(directory: String, file_ext: String, pattern: String) -
 
 /// This searches `directory` for files with extension `file_ext` returning a list of todos as 2-element vectors.
 #[defun]
-fn search_directory(env: &Env, directory: String, file_ext: String, pattern: String) -> emacs::Result<Value> {
+fn search_directory(
+    env: &Env,
+    directory: String,
+    file_ext: String,
+    pattern: String,
+) -> emacs::Result<Value> {
     let found = _search_directory(directory, file_ext, pattern)?;
 
     let elisp_results = found
         .into_iter()
-        .map(|todo| {
-            env.vector((todo.file_path, todo.line_number, todo.line))
-        })
+        .map(|todo| env.vector((todo.file_path, todo.line_number, todo.line)))
         .collect::<emacs::Result<Vec<Value>>>()?;
 
     env.vector(&elisp_results)
@@ -101,20 +108,29 @@ fn search_directory(env: &Env, directory: String, file_ext: String, pattern: Str
 
 #[cfg(test)]
 mod tests {
+    use crate::*;
     use std::fs::File;
     use std::io::Write;
     use tempfile::tempdir;
-    use crate::*;
 
     #[test]
     fn files_to_search_returns_the_expected_files() -> Result<(), String> {
-        let dir = tempdir().map_err(|e| format!("Unable to create a temporary directory: {:?}", e))?;
+        let dir =
+            tempdir().map_err(|e| format!("Unable to create a temporary directory: {:?}", e))?;
         let file_path = dir.path().join("my-temporary-file.md");
-        let mut file1 = File::create(&file_path).map_err(|e| format!("Unable to create a temp file: {:?}", e))?;
-        writeln!(file1, "Test data.").map_err(|e| format!("Unable to write to temp file: {:?}", e))?;
+        let mut file1 = File::create(&file_path)
+            .map_err(|e| format!("Unable to create a temp file: {:?}", e))?;
+        writeln!(file1, "Test data.")
+            .map_err(|e| format!("Unable to write to temp file: {:?}", e))?;
 
         let expected = vec![file_path.into_os_string()];
-        let actual = files_to_search(dir.into_path().into_os_string().to_string_lossy().to_string(), ".md".to_string());
+        let actual = files_to_search(
+            dir.into_path()
+                .into_os_string()
+                .to_string_lossy()
+                .to_string(),
+            ".md".to_string(),
+        );
         assert_eq!(expected, actual);
 
         Ok(())
@@ -122,13 +138,22 @@ mod tests {
 
     #[test]
     fn files_to_search_returns_no_files() -> Result<(), String> {
-        let dir = tempdir().map_err(|e| format!("Unable to create a temporary directory: {:?}", e))?;
+        let dir =
+            tempdir().map_err(|e| format!("Unable to create a temporary directory: {:?}", e))?;
         let file_path = dir.path().join("my-temporary-file.derp");
-        let mut file1 = File::create(&file_path).map_err(|e| format!("Unable to create a temp file: {:?}", e))?;
-        writeln!(file1, "Test data.").map_err(|e| format!("Unable to write to temp file: {:?}", e))?;
+        let mut file1 = File::create(&file_path)
+            .map_err(|e| format!("Unable to create a temp file: {:?}", e))?;
+        writeln!(file1, "Test data.")
+            .map_err(|e| format!("Unable to write to temp file: {:?}", e))?;
 
         let expected: Vec<OsString> = vec![];
-        let actual = files_to_search(dir.into_path().into_os_string().to_string_lossy().to_string(), ".md".to_string());
+        let actual = files_to_search(
+            dir.into_path()
+                .into_os_string()
+                .to_string_lossy()
+                .to_string(),
+            ".md".to_string(),
+        );
         assert_eq!(expected, actual);
 
         Ok(())
@@ -136,17 +161,29 @@ mod tests {
 
     #[test]
     fn search_directory_returns_the_expected_matches() -> Result<(), String> {
-        let dir = tempdir().map_err(|e| format!("Unable to create a temporary directory: {:?}", e))?;
+        let dir =
+            tempdir().map_err(|e| format!("Unable to create a temporary directory: {:?}", e))?;
         let file_path: std::path::PathBuf = dir.path().join("my-todo-file.md");
-        let mut file1 = File::create(&file_path).map_err(|e| format!("Unable to create a temp file: {:?}", e))?;
-        writeln!(file1, "- [ ] do your taxes").map_err(|e| format!("Unable to write to temp file: {:?}", e))?;
+        let mut file1 = File::create(&file_path)
+            .map_err(|e| format!("Unable to create a temp file: {:?}", e))?;
+        writeln!(file1, "- [ ] do your taxes")
+            .map_err(|e| format!("Unable to write to temp file: {:?}", e))?;
 
         let pattern = r"^- ?\[[Xx ]\] ".to_string();
-        let expected: Vec<TodoMatch> = vec![TodoMatch::new(file_path.into_os_string(), 1, "- [ ] do your taxes")];
+        let expected: Vec<TodoMatch> = vec![TodoMatch::new(
+            file_path.into_os_string(),
+            1,
+            "- [ ] do your taxes",
+        )];
         let actual = _search_directory(
-            dir.into_path().into_os_string().to_string_lossy().to_string(),
+            dir.into_path()
+                .into_os_string()
+                .to_string_lossy()
+                .to_string(),
             ".md".to_string(),
-            pattern).unwrap();
+            pattern,
+        )
+        .unwrap();
 
         assert_eq!(expected, actual);
 
